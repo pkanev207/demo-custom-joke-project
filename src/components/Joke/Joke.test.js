0be23d2fa8adc1b876a42dad359ca7cc4986d801
Petar <kanev_petar@yahoo.com>
2021-12-28 04:51:14 +0200

    added tests for Joke component

--- src/components/Joke/Joke.test.js
+++ src/components/Joke/Joke.test.js
@@ -1,7 +1,20 @@
-import Joke from './index';
+import { render, screen } from "@testing-library/react";
+// Component
+import Joke from "./index";
 
-describe('Joke Component', () => {
-    it('Should display content', () => {
-        expect(1).toBe(1);
-    });
+describe("Joke Component", () => {
+  it("Should display setup", () => {
+    render(<Joke data={{ setup: "test" }} />);
+    // expect(document.querySelector("span").textContent).toBe("test");
+    expect(document.getElementById("setup").textContent).toBe("test");
+  });
+  it("Should display setup and delivery", () => {
+    render(<Joke data={{ setup: "test", delivery: "test two" }} />);
+    expect(document.getElementById("setup").textContent).toBe("test");
+    expect(document.getElementById("delivery").textContent).toBe("test two");
+  });
+  it("Should display joke", () => {
+    render(<Joke data={{ joke: "test three" }} />);
+    expect(screen.getByTestId("joke").textContent).toBe("test three");
+  });
 });
